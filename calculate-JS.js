80b7ec377888ef5df97dd1d1ca7b0ad27496f975
victor335882 <victor335882@livemail.tw>
2017-01-05 20:35:18 +0800

    Add operate
    add two operator, “clearAll” and “back”

--- calculate-JS.js
+++ calculate-JS.js
@@ -1,11 +1,22 @@
 function digit(e) {
-  
     var displayDigit;
     var onScreen = document.getElementById("resultScreen").innerHTML;
     var addAfterClick = document.getElementsByName("digit"+e.value)[0].value;
-    
     displayDigit = onScreen + addAfterClick;  
-    
     document.getElementById("resultScreen").innerHTML = displayDigit;
-  
+    
+}
+
+function clearAll() {
+    var clear = "";
+    document.getElementById("resultScreen").innerHTML = clear;
+    
+}
+
+function back() {
+    var data = document.getElementById("resultScreen").innerHTML;
+    var dataLength = String(Number(String(data).length)-1);
+    var dataBack= data.substring(0,dataLength);
+    document.getElementById("resultScreen").innerHTML = dataBack;
+    
 }
