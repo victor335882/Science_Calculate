b521b930ae902e410bbed7e955e2f9d48337510e
victor335882 <victor335882@livemail.tw>
2017-01-21 21:28:45 +0800

    add power function
    remain :
    Add factorial function after “)” and delete it
    delete power10 function
    delete power function

--- calculate-JS.js
+++ calculate-JS.js
@@ -12,6 +12,7 @@ var front = {
         searchAns = onScreen.search("Ans");
         judgeLastString = onScreen.substring(onScreen.length - 1, onScreen.length);
         judgeLastPoint = onScreen.substring(onScreen.lastIndexOf(".") + 1, onScreen.length);
+        
         dataLength = Number(String(onScreen).length);
         
         // used to back functions
@@ -22,14 +23,18 @@ var front = {
         maxBgLfuncIndex = Math.max(onBackGround.lastIndexOf("+"), onBackGround.lastIndexOf("-"), onBackGround.lastIndexOf("*"), onBackGround.lastIndexOf("\/"), onBackGround.substring(0, onBackGround.length - 1).lastIndexOf("("));
         remainBgFunction = onBackGround.substring(0, maxBgLfuncIndex+1);
         
+        
+        
+        
         // used to factorial functions
         maxScLOperIndex = Math.max(onScreen.lastIndexOf("+"), onScreen.lastIndexOf("-"), onScreen.lastIndexOf("×"), onScreen.lastIndexOf("÷"), onScreen.lastIndexOf("("));
         //Due we already knowes the last string is numbers or not by (isNaN(judgeLastString)) condition in factorial functions, so add.replace(/!+/g, '') to reuse scAfLastOperatorTran in back function.
         scAfLastOperator = onScreen.substring(maxScLOperIndex+1, onScreen.length).replace(/!+/g, '');
         scAfLastOperatorTran = scAfLastOperator.replace(/\s+/g, '');
 
-        maxBgLOperIndex = Math.max(onBackGround.lastIndexOf("+"), onBackGround.lastIndexOf("-"), onBackGround.lastIndexOf("*"), onBackGround.lastIndexOf("\/"), onBackGround.lastIndexOf("("));
+        maxBgLOperIndex = Math.max(onBackGround.lastIndexOf("+"), onBackGround.lastIndexOf("-"), onBackGround.lastIndexOf("*"), onBackGround.lastIndexOf("\/"), onBackGround.lastIndexOf("("), onBackGround.lastIndexOf(","));
         bgBfLastOperator = onBackGround.substring(0, maxBgLOperIndex+1);
+        bgAfLastOperator = onBackGround.substring(maxBgLOperIndex+1, onBackGround.length)
 
         Fact = function(x) {
             var relevant=1;
@@ -38,6 +43,21 @@ var front = {
             return relevant;
         };
         
+        
+        //used to pow function
+        
+        var bgJudgeForPower = function() {
+            var i = 0;
+            do {
+                i += 1;
+                var aaa = onBackGround.substring(onBackGround.length - i, onBackGround.length);
+                var bgCountLeftPare = (aaa.match(/\(/g) || []).length;
+                var bgCountRightPare = (aaa.match(/\)/g) || []).length;
+                console.log(i);
+            } while (bgCountLeftPare < bgCountRightPare);
+            return onBackGround.length - i;
+
+        }
         
         //Used in rightPare function specially
         countLeftPare = (onScreen.match(/\(/g) || []).length;
@@ -67,11 +87,11 @@ function back() {
         frontData = onScreen.substring(0,dataLength - 4);
         backGroundData = onBackGround.substring(0,backDataLength - 3);
         
-    } else if( judgeLastString === "(" ) {
+    } else if( judgeLastString === "(") {
         frontData = remainScFunction;
         backGroundData = remainBgFunction;
         
-    } else if( judgeLastString === "!" ) {
+    } else if( judgeLastString === "!") {
         frontData = onScreen.substring(0,dataLength - 2);
         backGroundData = bgBfLastOperator + scAfLastOperatorTran;
         
@@ -84,9 +104,10 @@ function back() {
     
     console.log(document.getElementById("background").innerHTML);
 
+
 }
 
-function digit(e) { 
+function digit(e) {
     front.a();
     var addAfterClick = e.value;
     var addBackData = e.name;
@@ -259,31 +280,6 @@ function secFunc(e) {
     console.log(document.getElementById("background").innerHTML);
 }
 
-function power10(e) {
-    front.a();
-    var addAfterClick = e.value.substring(0,5) + " (";
-    var addBackData = e.name;
-    
-    if(judgeLastString === ".") {
-        frontData = onScreen+ "0 ×" + addAfterClick;
-        backGroundData = onBackGround + "0*" + addBackData;
-    } else if(onScreen.length === 0) {
-        frontData = addAfterClick;
-        backGroundData = addBackData;
-    } else if(judgeLastString === "+" || judgeLastString === "-" || judgeLastString === "×" || judgeLastString === "÷" || judgeLastString === "(") {
-        frontData = onScreen + addAfterClick;
-        backGroundData = onBackGround + addBackData;
-    } else {
-        frontData = onScreen + " ×" + addAfterClick;
-        backGroundData = onBackGround + "*" + addBackData;
-    }
-    document.getElementById("equqtion").innerHTML = frontData;
-    document.getElementById("background").innerHTML = backGroundData;
-    
-    console.log(document.getElementById("background").innerHTML);
-    
-}
-
 function powerExp(e) {
     front.a();
     var addAfterClick = e.value.substring(0,4) + " (";
@@ -312,9 +308,6 @@ function powerExp(e) {
 function factorial(e) {
     front.a();
     var addAfterClick = e.value;
-    var addBackData = e.name;
-    
-    
     
     if(isNaN(judgeLastString)) {
         // not number
@@ -333,15 +326,78 @@ function factorial(e) {
         frontData = onScreen + addAfterClick.substring(2,4);
         backGroundData = bgBfLastOperator + Fact(scAfLastOperatorTran);
     }
-        
     
     document.getElementById("equqtion").innerHTML = frontData;
     document.getElementById("background").innerHTML = backGroundData;
     
     console.log(document.getElementById("background").innerHTML);
+}
 
+function power10(e) {
+    front.a();
+    var addAfterClick = e.value.substring(0,5) + " (";
+    var addBackData = e.name;
+    if(onScreen.length === 0) {
+        frontData = addAfterClick;
+        backGroundData = addBackData;
+    } else if(judgeLastString === ".") {
+        frontData = onScreen+ " 0 ×" + addAfterClick;
+        backGroundData = onBackGround + "0*" + addBackData;
+        
+    } else if(judgeLastString === "+" || judgeLastString === "-" || judgeLastString === "×" || judgeLastString === "÷" || judgeLastString === "(") {
+        frontData = onScreen + addAfterClick;
+        backGroundData = onBackGround + addBackData;
+        
+    } else {
+        frontData = onScreen + " ×" + addAfterClick;
+        backGroundData = onBackGround + "*" + addBackData;
+    }
+    document.getElementById("equqtion").innerHTML = frontData;
+    document.getElementById("background").innerHTML = backGroundData;
     
+    console.log(document.getElementById("background").innerHTML);
+    
+}
 
+function power(e) {
+    front.a();
+    var addAfterClick = e.value.substring(2,4) + " (";
+    var addBackData = e.name;
+    
+    if(onScreen.length === 0) {
+        // not number
+        frontData = onScreen;
+        backGroundData = onBackGround;
+    } else if(isNaN(judgeLastString) && judgeLastString != "π" && judgeLastString != ")" && judgeLastString != ".") {
+        // not number
+        frontData = onScreen;
+        backGroundData = onBackGround;
+    } else if(isNaN(judgeLastString) === false || judgeLastString === "π") {
+        // not integer
+        frontData = onScreen + addAfterClick;
+        backGroundData = bgBfLastOperator + addBackData + bgAfLastOperator + ",";
+    } else if(judgeLastString === ".") {
+        // not integer
+        frontData = onScreen + " 0" + addAfterClick;
+        backGroundData = bgBfLastOperator + addBackData + bgAfLastOperator + "0,";
+    } else {
+        // Judge the special "(" to be the remaining bg data after press x^y or 10^y button
+        if(countLeftPare === countRightPare) {
+            frontData = onScreen + addAfterClick;
+            backGroundData = addBackData + onBackGround + ",";
+            
+        } else {
+            frontData = onScreen + addAfterClick;
+            backGroundData = onBackGround.substring(0,bgJudgeForPower()) + addBackData + onBackGround.substring(bgJudgeForPower(),onBackGround.length);
+        }
+        
+    } 
+    
+    document.getElementById("equqtion").innerHTML = frontData;
+    document.getElementById("background").innerHTML = backGroundData;
+    
+    console.log(document.getElementById("background").innerHTML);
+    
 }
 
 function equal(e) {
